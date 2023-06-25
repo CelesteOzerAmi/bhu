2dfa0e0f7fdc731589caf60cbe725d8612f30fea
Celeste <celeozer@hotmail.com>
2023-06-25 17:04:32 -0300

    se agregaron funciones en js

--- libros.js
+++ libros.js
@@ -1,7 +1,7 @@
 
 /* constructor */
 class Libro {
-    constructor(id, titulo, autor, editorial, año, disponible) {
+    constructor(id, titulo, autor, editorial, año) {
         this.id = id;
         this.titulo = titulo;
         this.autor = autor;
@@ -22,7 +22,6 @@ const libros = [libro1, libro2, libro3, libro4]
 /* array carrito */
 let carrito = [];
 
-
 /* DOM tarjeta 1  */
 
 const btn1 = document.getElementById("boton1")
@@ -34,7 +33,6 @@ btn1.addEventListener("click", () => {
         carrito.push(libro1);
         crearColeccion();
     }
-    btn1.innerHTML = "libro en mi colección"
 })
 
 /* DOM tarjeta 2 */
@@ -48,7 +46,6 @@ btn2.addEventListener("click", () => {
         carrito.push(libro2);
         crearColeccion();
     }
-    btn2.innerHTML = "libro en mi colección"
 })
 
 /* DOM tarjeta 3 */
@@ -62,7 +59,6 @@ btn3.addEventListener("click", () => {
         carrito.push(libro3);
         crearColeccion();
     }
-    btn3.innerHTML = "libro en mi colección"
 })
 
 /* DOM tarjeta 4 */
@@ -76,16 +72,29 @@ btn4.addEventListener("click", () => {
     } else {
         carrito.push(libro4);
         crearColeccion();
-    } btn4.innerHTML = "libro en mi colección"
+    }
 })
 
 /* DOM carrito */
 
 let coleccion = document.getElementById("coleccion")
 
+/* storage carrito ERROR 
+
+const setStorage = (coleccion) => {
+    sessionStorage.setItem("coleccion", JSON.stringify(coleccion))
+}
+
+if (sessionStorage.getItem("coleccion")) {
+    coleccion = JSON.parse(sessionStorage.getItem("coleccion"))
+    crearColeccion()
+} else { setStorage() }*/
+
 /* funcion para añadir producto al carrito */
 
+
 function crearColeccion() {
+    editarTex()
     coleccion.innerHTML = "";
     coleccion.classList.add("card-group");
     carrito.forEach((libro) => {
@@ -93,28 +102,47 @@ function crearColeccion() {
         carritocontainer.classList.add("card");
         carritocontainer.innerHTML = `<section id="section"> <h3> "${libro.titulo}", por ${libro.autor}.</h3> <p>editorial: ${libro.editorial}</p> <small>año de publicación: ${libro.año}</small> <a class="btn btn-dark" onClick="eliminarDeColeccion(${libro.id})">quitar de la colección</a></section>`
         coleccion.appendChild(carritocontainer)
+        /*setStorage()*/
     })
+}
+
+/* función para editar texto en botones de tarjetas */
+
+function editarTex() {
+    if (carrito.find(({ id }) => id === libro1.id)) {
+        btn1.innerHTML = "libro en mi colección"
+    } else {
+        btn1.innerHTML = "añadir a mi colección"
+    }
+    if (carrito.find(({ id }) => id === libro2.id)) {
+        btn2.innerHTML = "libro en mi colección"
+    } else {
+        btn2.innerHTML = "añadir a mi colección"
+    }
+    if (carrito.find(({ id }) => id === libro3.id)) {
+        btn3.innerHTML = "libro en mi colección"
+    } else {
+        btn3.innerHTML = "añadir a mi colección"
+    }
+    if (carrito.find(({ id }) => id === libro4.id)) {
+        btn4.innerHTML = "libro en mi colección"
+    } else {
+        btn4.innerHTML = "añadir a mi colección"
+    }
 }
 
 /* funcion para eliminar producto del carrito */
 
 function eliminarDeColeccion(id) {
     const producto = carrito.find((producto) => producto.id === id);
-    carrito.splice(carrito.indexOf(producto), 1);
+    carrito.splice(carrito.indexOf(producto), 1)
+        ;
     if (carrito.length < 1) {
         coleccion.innerHTML = "<h4>sin libros en esta colección :(</h4>"
     } else {
         crearColeccion()
     }
+    editarTex()/*
+    setStorage()*/
 }
 
-
-/*
-if (localStorage.getItem("carrito")){
-    carrito = JSON.parse(localStorage.getItem("carrito"))
-} else {setStorage()}
-
-
-const setStorage = (carrito) => { 
-localStorage.setItem("carrito", JSON.stringify(carrito))}
-*/
